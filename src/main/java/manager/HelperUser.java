d9555bca3025b8b6553b834abbdc82bdf7e432e0
LioraOceacovschi <liora.skliar@gmail.com>
2023-07-08 19:43:51 +0300

    HW

--- src/main/java/manager/HelperUser.java
+++ src/main/java/manager/HelperUser.java
@@ -1,9 +1,39 @@
 package manager;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class HelperUser extends HelperBase{
+public class HelperUser extends HelperBase {
+
+
     public HelperUser(WebDriver wd) {
         super(wd);
+    }
+
+    public void openLoginForm() {
+        click(By.xpath("//a[text()=' Log in ']"));
+    }
+    public boolean isLogged(){
+       return isElementPresent(By.xpath("//a[.=' Logout ']"));
+    }
+    public void openRegistrationForm() {
+        click(By.xpath("//a[text()=' Sign up ']"));
+    }
+
+    public void fillLoginForm(String email, String password) {
+        type(By.xpath("//input[@id='email']"), email);
+        type(By.xpath("//input[@id='password']"), password);
+    }
+
+    public void submitLogin() {
+        click(By.xpath("//button[contains(.,\"alla!\")]"));
+    }
+
+    public void clickOkButton() {
+        click(By.xpath("//button[.='Ok']"));
+    }
+
+    public void logOut() {
+        click(By.xpath("//a[.=' Logout ']"));
     }
 }
--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,11 +1,32 @@
 package tests;
 
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class LoginTests extends TestBase{
+public class LoginTests extends TestBase {
+
+    @BeforeMethod
+    public void preConditions(){
+        if(app.getUser().isLogged()){
+            app.getUser().logOut();
+        }
+    }
 
     @Test
-    public void loginPositive(){
+    public void loginPositive() {
+        app.getUser().openLoginForm();
+        app.getUser().fillLoginForm("liora@gmail.com", "$Liora12345");
+        app.getUser().submitLogin();
+        Assert.assertTrue(app.getUser().isElementPresent(By.xpath("//h1[.='Logged in']")));
+        app.getUser().clickOkButton();
+        Assert.assertTrue(app.getUser().isLogged());
+    }
+    @AfterMethod
+    public void postCondition(){
 
     }
+
 }
